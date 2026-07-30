249dca224afb29e339fd095609a62653fa6b4035
Knighscorpio <oluwagbotemi4god@gmail.com>
2026-07-30 20:10:07 +0100

    Fix base path for GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/joshdesignz/',  
+export default defineConfig(({ command, mode }) => {
+  
+  const isGitHubPages = process.env.GITHUB_PAGES || false;
+  const base = isGitHubPages ? '/joshdesignz/' : '/';
+  return {
+    plugins: [react()],
+    base: base,
+  };
 });
